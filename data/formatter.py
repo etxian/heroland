c687ceeede614054180c47bb7233091da80df961
zhany <zhany@unity3d.com>
2017-06-20 19:31:01 -0700

    Add json formatter

---
+++ data/formatter.py
@@ -0,0 +1,18 @@
+#-*-coding: utf-8-*-
+from __future__ import unicode_literals
+import json
+import glob
+import sys
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+files = glob.glob("*.json")
+
+for f in files:
+    content = {}
+    with open(f, 'r') as input:
+        content = json.load(input)
+    with open(f, "w") as output:
+        formatted = json.dumps(content, indent=2, ensure_ascii=False).encode('utf-8')
+        output.write(formatted)
